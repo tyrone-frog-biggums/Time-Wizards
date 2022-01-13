d86ca6f0eae34982203ebc02767211bf7eada65b
Ryan Gorman <93864117+Memetokens@users.noreply.github.com>
2022-01-13 10:44:47 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ export const StyledButton = styled.button`
   padding: 10px;
   font-weight: bold;
   color: var(--secondary-text);
-  width: 100px;
+  width: 120px;
   cursor: pointer;
   box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
   -webkit-box-shadow: 0px 6px 0px -2px rgba(250, 250, 250, 0.3);
